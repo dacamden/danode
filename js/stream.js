08eecc5f1c5d7067dca8e0959f4feef4d47a6dee
digital-alchemy <amly@hotmail.co.uk>
2014-07-08 21:56:06 +0000

    edits

---
+++ js/stream.js
@@ -0,0 +1,19 @@
+var twit = require ('twit');
+var key = require ('key.js')
+
+var twitter = new twit (keys);
+
+var stream = 
+twitter.stream  
+  ('statuses/filter', 
+    {
+      track: 'mango'
+    }
+  );
+
+stream.on 
+  ('tweet', function (tweet) 
+    {
+      console.log (tweet)
+    }
+  );
