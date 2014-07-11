5f25b9ae7934077626456ea4933b75c9e2ce40b1
Abi <0c0-Abi@users.noreply.github.com>
2014-07-11 01:06:46 +0100

    Update and rename draft.js to server.js

---
+++ drafts/theo/server.js
@@ -0,0 +1,57 @@
+var twitter = require('twit');
+var fs = require('fs');
+var http = require('http');
+
+var jsonPacket = []
+    //
+    // filter the public stream by english tweets containing `#apple`
+    //
+var stream = twitter.stream('statuses/filter', {
+    track: 'sunshine',
+    language: 'en',
+    /*location: 'London'*/
+})
+stream.on('tweet', function(tweet) {
+    
+    if (tweet.entities.media === undefined) {} else {
+        var url = tweet.entities.media[0].media_url
+        var id = tweet.user.screen_name
+        
+        
+            //console.log(tweetobject)
+        fs.readFile('tweetLog.json', function read(err, data) {
+            if (err) {
+                throw err;
+            }
+           var singleTweet = JSON.stringify(
+               {"Tweet":{"id":id,"url":url}}, null, 4)
+//            {
+                
+//                 console.log('New tweet');
+//             };
+           fs.readFile('tweetLog.json', function(err,data){
+             //console.log(data)
+              var allTweets = JSON.parse(data);
+              allTweets.push(singleTweet)
+                 JSON.stringify(allTweets)
+              fs.writeFile('tweetLog.json',allTweets, function (err){
+                 if (err) throw err; 
+                 console.log('new tweet')
+                 
+              })
+           })
+        });
+    }
+});
+http.createServer(function(request, response) {
+    fs.readFile('tweetLog.json', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        response.writeHead(200, {
+            'Content-Type': 'text/json',
+            'Access-Control-Allow-Origin': '*'
+        });
+        response.end(data);
+    })
+}).listen(3000);
