301786cd4244234d7455f3192424ccf68d4864da
Abi <0c0-Abi@users.noreply.github.com>
2014-07-11 12:17:46 +0100

    Update server.js

--- drafts/abi/server.js
+++ drafts/abi/server.js
@@ -3,17 +3,3 @@ var twit = require ('twit');
 var key = require ('key.js');
 var twitter = new twit (keys);
 
-var stream = 
-twitter.stream  
-  ('statuses/filter', 
-    {
-      track: 'mango'
-    }
-  );
-
-stream.on 
-  ('tweet', function (tweet) 
-    {
-      console.log (tweet)
-    }
-  );
