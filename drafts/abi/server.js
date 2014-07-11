f8a6a9aad3fdd880eab10ecd73fb64604d9294fc
Abi <0c0-Abi@users.noreply.github.com>
2014-07-11 12:25:33 +0100

    Update server.js

--- drafts/abi/server.js
+++ drafts/abi/server.js
@@ -1,5 +1,21 @@
+/*GLOBAL VARIABLES*/ //2,3,4,5 
 var http = require ('http');
 var twit = require ('twit');
 var key = require ('key.js');
 var twitter = new twit (keys);
 
+
+
+/*SERVER*/ //10
+http.createServer(function(request, response) {
+    fs.readFile('tweetLog.json', function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        response.writeHead(200, {
+            'Content-Type': 'text/json',
+            'Access-Control-Allow-Origin': '*'
+        });
+        response.end(data);
+    })
+}).listen(3000);
